7639203f2fdd77b19a89faa719c7e1f038fb8107
Beemnet <aquacoder00100011@gmail.com>
2025-12-25 21:55:38 +0300

    added basic controls

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/VideoMode.hpp>
 #include <filesystem>
 #include <fstream>
 
@@ -85,136 +93,15 @@ Mesh create_cube_mesh() {
   return currentObj;
 }
 
-void countObj(const char *path, int &vertexCount, int &faceCount) {
-  std::ifstream file(path);
-  std::string line;
-
-  vertexCount = 0;
-  faceCount = 0;
-
-  while (std::getline(file, line)) {
-    if (line[0] == 'v' && line[1] == ' ')
-      vertexCount++;
-    else if (line[0] == 'f' && line[1] == ' ')
-      faceCount++;
-  }
+void delete_mesh(Mesh &mesh) {
+  delete[] mesh.tri_list;
+  mesh.tri_count = 0;
 }
 
-Mesh loadObj(const char *path) {
-  int vertexCount = 0;
-  int faceCount = 0;
-
-  countObj(path, vertexCount, faceCount);
-
-  Vec3 *vertices = new Vec3[vertexCount];
-  Triangle *triangles = new Triangle[faceCount];
-
-  std::ifstream file(path);
-  std::string line;
-
-  int vIndex = 0;
-  int fIndex = 0;
-
-  while (std::getline(file, line)) {
-    if (line[0] == 'v' && line[1] == ' ') {
-      std::istringstream iss(line);
-      char type;
-
-      iss >> type >> vertices[vIndex].x >> vertices[vIndex].y >>
-          vertices[vIndex].z;
-
-      ++vIndex;
-    }
-
-    else if (line[0] == 'f' && line[1] == ' ') {
-      std::istringstream iss(line);
-      char type;
-      int a, b, c;
-
-      iss >> type >> a >> b >> c;
-
-      triangles[fIndex].points[0] = vertices[a - 1];
-      triangles[fIndex].points[1] = vertices[b - 1];
-      triangles[fIndex].points[2] = vertices[c - 1];
-
-      ++fIndex;
-    }
-  }
-  Vec3 min = vertices[0], max = vertices[0];
-  
-  for (int i = 1; i < vertexCount; i++) {
-    if (vertices[i].x < min.x)
-      min.x = vertices[i].x;
-    if (vertices[i].y < min.y)
-      min.y = vertices[i].y;
-    if (vertices[i].z < min.z)
-      min.z = vertices[i].z;
-
-    if (vertices[i].x > max.x)
-      max.x = vertices[i].x;
-    if (vertices[i].y > max.y)
-      max.y = vertices[i].y;
-    if (vertices[i].z > max.z)
-      max.z = vertices[i].z;
-  }
-
-  Vec3 center = {(min.x + max.x) * 0.5f, (min.y + max.y) * 0.5f,
-                 (min.z + max.z) * 0.5f};
-  float scale = std::max({max.x - min.x, max.y - min.y, max.z - min.z});
-  if (scale != 0.f)
-    scale = 1.f / scale;
-
-  for (int i = 0; i < vertexCount; i++) {
-    vertices[i].x = (vertices[i].x - center.x) * scale;
-    vertices[i].y = (vertices[i].y - center.y) * scale;
-    vertices[i].z = (vertices[i].z - center.z) * scale;
-  }
-
-  // Update triangles with normalized vertices
-  for (int i = 0; i < faceCount; i++) {
-    triangles[i].points[0].x = (triangles[i].points[0].x - center.x) * scale;
-    triangles[i].points[0].y = (triangles[i].points[0].y - center.y) * scale;
-    triangles[i].points[0].z = (triangles[i].points[0].z - center.z) * scale;
-
-    triangles[i].points[1].x = (triangles[i].points[1].x - center.x) * scale;
-    triangles[i].points[1].y = (triangles[i].points[1].y - center.y) * scale;
-    triangles[i].points[1].z = (triangles[i].points[1].z - center.z) * scale;
-
-    triangles[i].points[2].x = (triangles[i].points[2].x - center.x) * scale;
-    triangles[i].points[2].y = (triangles[i].points[2].y - center.y) * scale;
-    triangles[i].points[2].z = (triangles[i].points[2].z - center.z) * scale;
-  }
-
-  delete[] vertices;
-
-  Mesh mesh;
-  mesh.tri_list = triangles;
-  mesh.tri_count = faceCount;
-
-  return mesh;
-}
-
-
-
-
-int main() {
-  sf::RenderWindow window(sf::VideoMode(1000,1000), "title");
-  sf::Event event;
-
-  float width = window.getSize().x;
-  float height = window.getSize().y;
-
-  std::string path = std::filesystem::current_path().string() + "//rsrc//Lagtrain.obj";
-  auto cube = loadObj(path.c_str());
-  std::cout << cube.tri_count << std::endl;
-
+Mat4x4 getProjectionMatrix(float aspect_ratio, float fov, float zNear,
+                           float zFar) {
   Mat4x4 projection_matrix;
-  float aspect_ratio = (float)height / width;
-  float fov = 90 * 0.5 / 180 * PI;
   float fov_angle = 1 / tan(fov);
-
-  float zNear = 0.1f;
-  float zFar = 1000.f;
 
   projection_matrix.m[0][0] = aspect_ratio * fov_angle;
   projection_matrix.m[1][1] = fov_angle;
@@ -222,72 +109,198 @@ int main() {
   projection_matrix.m[3][2] = (zFar * zNear) / (zFar - zNear);
   projection_matrix.m[2][3] = 1;
 
-  Mat4x4 rotation_z;
-  Mat4x4 rotation_x;
+  return projection_matrix;
+}
+
+void update_rotation_matrix_y(Mat4x4 &rotation_y, float theta) {
+  rotation_y.m[0][0] = cosf(theta);
+  rotation_y.m[0][2] = -sinf(theta);
+  rotation_y.m[1][1] = 1.f;
+  rotation_y.m[2][0] = sinf(theta);
+  rotation_y.m[2][2] = cosf(theta);
+  rotation_y.m[3][3] = 1.f;
+}
+
+int main() {
+  sf::RenderWindow window(sf::VideoMode(1000, 1000), "title");
+  window.setVerticalSyncEnabled(true);
+
+  sf::Event event;
+
+  float width = window.getSize().x;
+  float height = window.getSize().y;
+
+  // mesh information
+  Mesh cube = create_cube_mesh();
+  sf::VertexArray mesh_vertex(sf::LinesStrip, cube.tri_count * 3);
+  sf::CircleShape *mesh_points = new sf::CircleShape[cube.tri_count * 3];
+
+  // projection matrix init
+  float aspect_ratio = (float)height / width;
+  float fov = 90 * 0.5 / 180 * PI;
+  float zNear = 0.1f;
+  float zFar = 1000.f;
+  Mat4x4 projection_matrix =
+      getProjectionMatrix(aspect_ratio, fov, zNear, zFar);
+
+  // rotation matrix
+  Mat4x4 rotation_y;
+  float theta = 0.f;
 
   sf::Clock clock;
   sf::Time timer;
-  float theta = 0.f;
 
+  float translation_dist = 3.f;
+  float translation_y = 0.f;
+  
+  constexpr float input_update = 25.f;
+  
   while (window.isOpen() == true) {
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         window.close();
     }
     timer += clock.restart();
-    if (timer.asMilliseconds() > 25.f) {
-      theta += 0.02;
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::E) &&
+        timer.asMilliseconds() > input_update) {
+      theta += 0.01;
       timer = sf::Time::Zero;
     }
-    window.clear();
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q) &&
+        timer.asMilliseconds() > input_update) {
+      theta -= 0.01;
+      timer = sf::Time::Zero;
+    }
 
-    rotation_z.m[0][0] = cosf(theta);
-    rotation_z.m[0][1] = sinf(theta);
-    rotation_z.m[1][0] = -sinf(theta);
-    rotation_z.m[1][1] = cosf(theta);
-    rotation_z.m[2][2] = 1.f;
-    rotation_z.m[3][3] = 1.f;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) &&
+        timer.asMilliseconds() > input_update) {
+      translation_dist -= 0.01;
+      timer = sf::Time::Zero;
+    }
 
-    rotation_x.m[0][0] = 1.f;
-    rotation_x.m[1][1] = cosf(theta * 0.5f);
-    rotation_x.m[1][2] = sinf(theta * 0.5f);
-    rotation_x.m[2][1] = -sinf(theta * 0.5f);
-    rotation_x.m[2][2] = cosf(theta * 0.5f);
-    rotation_x.m[3][3] = 1.f;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) &&
+        timer.asMilliseconds() > input_update) {
+      translation_dist += 0.01;
+      timer = sf::Time::Zero;
+    }
 
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) &&
+        timer.asMilliseconds() > input_update) {
+      translation_y += 0.01;
+      timer = sf::Time::Zero;
+    }
+    
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) &&
+        timer.asMilliseconds() > input_update) {
+      translation_y -= 0.01;
+      timer = sf::Time::Zero;
+    }
+
+    update_rotation_matrix_y(rotation_y, theta);
+    
+    int mesh_vertex_index = 0;
     for (int i = 0; i < cube.tri_count; i++) {
       Triangle curr_tri = cube.tri_list[i];
       Triangle proj_tri;
-      float translation_dist = 1.f;
 
       for (int j = 0; j < 3; j++) {
-        curr_tri.points[j] = MatMul(rotation_z, curr_tri.points[j]);
-        curr_tri.points[j] = MatMul(rotation_x, curr_tri.points[j]);
+        curr_tri.points[j] = MatMul(rotation_y, curr_tri.points[j]);
 
         curr_tri.points[j].z += translation_dist;
-
         proj_tri.points[j] = MatMul(projection_matrix, curr_tri.points[j]);
+
+        // normalization
         proj_tri.points[j].x += 1.f;
-        proj_tri.points[j].y += 1.f;
         proj_tri.points[j].x *= 0.5f * width;
-        proj_tri.points[j].y *= 0.5f * height;
+
+        proj_tri.points[j].y += 1.f;
+        proj_tri.points[j].y *= 0.5f;
+        proj_tri.points[j].y = (1 - proj_tri.points[j].y);
+        proj_tri.points[j].y *= width;
       }
-      sf::Vertex lines[6] = {
+      float radius = 10.f;
+
+      for (int i = 0; i < 3; ++i) {
+        mesh_points[mesh_vertex_index + i].setRadius(radius);
+        mesh_points[mesh_vertex_index + i].setOrigin(radius, radius);
+      }
+      mesh_points[mesh_vertex_index + 0].setPosition(proj_tri.points[0].x,
+                                                     proj_tri.points[0].y);
+
+      mesh_points[mesh_vertex_index + 1].setPosition(proj_tri.points[1].x,
+                                                     proj_tri.points[1].y);
+
+      mesh_points[mesh_vertex_index + 2].setPosition(proj_tri.points[2].x,
+                                                     proj_tri.points[2].y);
+
+      mesh_vertex[mesh_vertex_index] =
           sf::Vertex(sf::Vector2f(proj_tri.points[0].x, proj_tri.points[0].y),
-                     sf::Color::White),
+                     sf::Color::White);
+
+      mesh_vertex[mesh_vertex_index + 1] =
           sf::Vertex(sf::Vector2f(proj_tri.points[1].x, proj_tri.points[1].y),
-                     sf::Color::White),
-          sf::Vertex(sf::Vector2f(proj_tri.points[1].x, proj_tri.points[1].y),
-                     sf::Color::White),
+                     sf::Color::White);
+
+      mesh_vertex[mesh_vertex_index + 2] =
           sf::Vertex(sf::Vector2f(proj_tri.points[2].x, proj_tri.points[2].y),
-                     sf::Color::White),
-          sf::Vertex(sf::Vector2f(proj_tri.points[2].x, proj_tri.points[2].y),
-                     sf::Color::White),
-          sf::Vertex(sf::Vector2f(proj_tri.points[0].x, proj_tri.points[0].y),
-                     sf::Color::White)};
-      window.draw(lines, 6, sf::Points);
+                     sf::Color::White);
+
+      mesh_vertex_index += 3;
     }
+
+    window.clear();
+    window.draw(mesh_vertex);
+    for (int i = 0; i < cube.tri_count * 3; i++)
+      window.draw(mesh_points[i]);
     window.display();
   }
+  delete_mesh(cube);
+  delete[] mesh_points;
 }
 
+// int main_2() {
+//   sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+//   window.setVerticalSyncEnabled(true);
+
+//   sf::CircleShape red(20.f);
+//   red.setFillColor(sf::Color::Red);
+//   red.setPosition(window.getPosition().x / 2, window.getPosition().y / 2);
+//   red.setOrigin(red.getRadius(), red.getRadius());
+
+//   bool mouse_press = false;
+//   bool moving = false;
+
+//   while (window.isOpen()) {
+//     sf::Event event;
+//     while (window.pollEvent(event)) {
+//       if (event.type == sf::Event::Closed)
+//         window.close();
+
+//       if (event.type == sf::Event::MouseButtonPressed) {
+//         if (event.mouseButton.button == sf::Mouse::Left) {
+//           mouse_press = true;
+//         }
+//       }
+
+//       if (event.type == sf::Event::MouseButtonReleased) {
+//         if (event.mouseButton.button == sf::Mouse::Left) {
+//           mouse_press = false;
+//           moving = false;
+//         }
+//       }
+//     }
+//     sf::Vector2f mousePos =
+//         static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
+//     if(red.getGlobalBounds().contains(mousePos) && mouse_press == true){
+//       moving = true;
+//     }
+//     if(moving == true)
+//       red.setPosition(mousePos);
+//     std::cout << moving <<  "  " << mouse_press << std::endl;
+
+//     window.clear(sf::Color::Black);
+//     window.draw(red);
+//     window.display();
+//   }
+// }
